95b113aeb2cd70af3678b7f9b3631730fb9d645c
jun-gyu <57890052+jun-gyu@users.noreply.github.com>
2020-02-14 23:26:35 +0900

    14
    14

--- edu/clock.js
+++ edu/clock.js
@@ -1,6 +1,19 @@
-const form = document.querySelector(".js-form"),
-  input = form.querySelector("input");
+const clockContainer = document.querySelector(".js-clock"),
+  clockTitle = clockContainer.querySelector("h1");
 
-function init() {}
+function getTime() {
+  const date = new Date();
+  const minutes = date.getMinutes();
+  const hours = date.getHours();
+  const seconds = date.getSeconds();
+  clockTitle.innerText = `${hours < 10 ? `0${hours}` : hours}:${
+    minutes < 10 ? `0${minutes}` : minutes
+  }:${seconds < 10 ? `0${seconds}` : seconds}`;
+}
 
-init;
+function init() {
+  getTime();
+  setInterval(getTime, 1000);
+}
+
+init();
